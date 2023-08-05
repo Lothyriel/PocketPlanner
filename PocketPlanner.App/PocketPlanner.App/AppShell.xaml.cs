b5a075cf6926a2b59bdd00bb0a00c6d8f89cd8da
fastjonh <fastjonh@gmail.com>
2023-08-05 18:50:48 -0300

    adding .Net Maui project

---
+++ PocketPlanner.App/PocketPlanner.App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PocketPlanner.App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PocketPlanner.App/PocketPlanner.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PocketPlanner.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
