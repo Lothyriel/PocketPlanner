3e8464eed5078582027384c029bb999d0c4e05b8
fastjonh <fastjonh@gmail.com>
2023-08-08 21:33:47 -0300

    -finished tests
    -extracting tested module to own folder

---
+++ PocketPlanner.Api/tests/picpay_extraction.rs
@@ -0,0 +1,29 @@
+#[cfg(test)]
+mod tests {
+    use std::{fs::File, str::FromStr};
+
+    use anyhow::Error;
+    use chrono::Datelike;
+    use rust_decimal::Decimal;
+
+    use pocket_planner::application::extractors::{picpay::Picpay, CreditCardExtractor};
+
+    #[test]
+    fn picpay_extraction() -> Result<(), Error> {
+        let file = File::open("tests/data/picpay_fatura_teste.pdf")?;
+
+        let info = Picpay::extract_entries(file, chrono::Utc::now().year())?;
+
+        assert_eq!(info.len(), 2);
+
+        let globo = &info[0];
+        assert_eq!((globo.date.day(), globo.date.month()), (23, 04));
+        assert_eq!(globo.value, Decimal::from_str("29.90")?);
+
+        let amazon = &info[1];
+        assert_eq!((amazon.date.day(), amazon.date.month()), (19, 07));
+        assert_eq!(amazon.value, Decimal::from_str("14.90")?);
+
+        Ok(())
+    }
+}
