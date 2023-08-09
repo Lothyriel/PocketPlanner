d6069e185df4829d2e2831f02447fe2c80024015
fastjonh <fastjonh@gmail.com>
2023-08-08 22:31:54 -0300

    extracted method to extensions

--- PocketPlanner.Api/src/application/model/credit_card.rs
+++ PocketPlanner.Api/src/application/model/credit_card.rs
@@ -1,8 +1,8 @@
-use std::str::FromStr;
-
-use anyhow::{anyhow, Error};
+use anyhow::Error;
 use chrono::NaiveDate;
 use rust_decimal::Decimal;
+
+use crate::extensions::chrono::NaiveDateExt;
 
 pub struct CreditCardEntry {
     pub date: NaiveDate,
@@ -12,16 +12,8 @@ pub struct CreditCardEntry {
 
 impl CreditCardEntry {
     pub fn try_from_chunk(date: &str, description: &str, value: &str) -> Result<Self, Error> {
-        let segments: Vec<_> = date.split('/').collect();
-
-        if segments.len() != 3 {
-            return Err(anyhow!("Invalid date {}", date));
-        }
-
-        let date = format!("{}-{}-{}", segments[2], segments[1], segments[0]);
-
         Ok(Self {
-            date: NaiveDate::from_str(&date)?,
+            date: NaiveDate::from_str_pt(date, '/')?,
             description: description.to_owned(),
             value: Decimal::from_str_exact(&value.replace(",", "."))?,
         })
