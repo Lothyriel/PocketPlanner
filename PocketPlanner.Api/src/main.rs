a06b119280eb3cd506eefdb292ec8a5e316a404b
fastjonh <fastjonh@gmail.com>
2023-08-09 20:36:01 -0300

    no unwrap...

--- PocketPlanner.Api/src/main.rs
+++ PocketPlanner.Api/src/main.rs
@@ -12,7 +12,7 @@ async fn start() -> Result<(), Error> {
 
     let app = Router::new();
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
     Ok(axum::serve(listener, app).await?)
 }
