9aa94031a7a458dcbad045dfb4d7df7fd62ddc8f
fastjonh <fastjonh@gmail.com>
2023-10-01 15:45:58 -0300

    ditching .NET Maui...
    fica pra proxima, vou unir o util ao agradavel e fazer o trabalho de mobile junto com esse app

--- PocketPlanner.App/PocketPlanner.App/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PocketPlanner.App;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- PocketPlanner.App/PocketPlanner.App/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace PocketPlanner.App;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
