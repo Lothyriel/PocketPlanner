f60a770d12dad6598d895eaaa3edc4a6dd8f9d46
fastjonh <fastjonh@gmail.com>
2023-10-01 23:27:39 -0300

    adding search input in Summary

--- PocketPlanner.App/Screens/Summary.tsx
+++ PocketPlanner.App/Screens/Summary.tsx
@@ -1,7 +1,56 @@
-import { View } from "react-native"
+import { View, StyleSheet } from "react-native"
+import { TextInput } from 'react-native-paper';
+import { colors } from "../colors"
 
 export default function() {
   return (
-    <View></View>
+    <View style={styles.container}>
+      <View style={styles.search}>
+        <Search />
+      </View>
+      <View style={styles.invoices}></View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  search: {
+    flex: 1,
+    padding: '4%',
+  },
+  invoices: {
+    backgroundColor: 'grey',
+    flex: 12,
+  }
+})
+
+function Search() {
+  const lightGrey = 'rgb(239, 240,244)'
+  const grey = 'rgb(139, 139, 139)'
+
+  const styles = StyleSheet.create({
+    text: {
+      flex: 1,
+      fontSize: 20,
+      backgroundColor: 'white'
+    }
+  })
+
+  return (
+    <TextInput
+      style={styles.text}
+      placeholder={"Buscar item"}
+      onChangeText={() => { }}
+      mode="outlined"
+      activeOutlineColor={colors.main}
+      outlineColor={lightGrey}
+      placeholderTextColor={grey}
+      outlineStyle={{ borderRadius: 10, borderWidth: 3 }}
+      right={<TextInput.Icon icon="magnify" color={grey} />}
+    />
   )
 }
