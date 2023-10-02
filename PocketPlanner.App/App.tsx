207701f8738ed8fde5d5fb8a4d0d2451f13281bc
fastjonh <fastjonh@gmail.com>
2023-10-01 23:27:18 -0300

    adding some header spacing

--- PocketPlanner.App/App.tsx
+++ PocketPlanner.App/App.tsx
@@ -1,6 +1,8 @@
 import { BottomTabNavigationProp, createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { FontAwesome5 } from '@expo/vector-icons'
+import { View } from 'react-native'
+import { colors } from './colors'
 import Summary from './Screens/Summary'
 import Extract from './Screens/Extract'
 
@@ -8,16 +10,21 @@ const Tab = createBottomTabNavigator<Routes>()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator backBehavior='history' screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Summary" component={Summary} options={{
-          tabBarIcon: () => <FontAwesome5 name="receipt" size={24} color="black" />
-        }} />
-        <Tab.Screen name="Extract" component={Extract} options={{
-          tabBarIcon: () => <FontAwesome5 name="file-import" size={24} color="black" />
-        }} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: colors.main }} />
+      <View style={{ flex: 35 }}>
+        <NavigationContainer>
+          <Tab.Navigator backBehavior='history' screenOptions={{ headerShown: false }}>
+            <Tab.Screen name="Summary" component={Summary} options={{
+              tabBarIcon: () => <FontAwesome5 name="receipt" size={24} color="black" />
+            }} />
+            <Tab.Screen name="Extract" component={Extract} options={{
+              tabBarIcon: () => <FontAwesome5 name="file-import" size={24} color="black" />
+            }} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </View>
+    </View >
   )
 }
 
