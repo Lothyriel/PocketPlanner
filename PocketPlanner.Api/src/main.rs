62d5cebd9a732e8948681390f12519d58206ab2b
fastjonh <fastjonh@gmail.com>
2023-10-18 22:49:04 -0300

    .

--- PocketPlanner.Api/src/main.rs
+++ PocketPlanner.Api/src/main.rs
@@ -1,18 +1,21 @@
-use anyhow::Error;
-use axum::Router;
-use pocket_planner::extensions::log_ext::LogExt;
+use axum::{routing, Router};
+use log::error;
 
 #[tokio::main]
 async fn main() {
-    start().await.log_err();
+    if let Err(err) = start().await {
+        error!(format!("{}", err.to_string()));
+    }
 }
 
-async fn start() -> Result<(), Error> {
+async fn start() -> Result<(), std::io::Error> {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new();
+    let app = Router::new().route("/health", routing::get(|| async { "Estou saudável" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
     Ok(axum::serve(listener, app).await?)
 }
+
+enum ResponseError {}
