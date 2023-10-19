a600f1e35944c41e43b3a3263f45e590464eda9a
fastjonh <fastjonh@gmail.com>
2023-10-18 22:49:26 -0300

    anyhow > thiserror

--- PocketPlanner.Api/src/extensions/chrono.rs
+++ PocketPlanner.Api/src/extensions/chrono.rs
@@ -1,18 +1,19 @@
 use std::str::FromStr;
 
-use anyhow::{anyhow, Error};
 use chrono::NaiveDate;
 
+use crate::application::extractors::ParsingError;
+
 pub trait NaiveDateExt {
-    fn from_str_pt(date: &str, separator: char) -> Result<NaiveDate, Error>;
+    fn from_str_pt(date: &str, separator: char) -> Result<NaiveDate, ParsingError>;
 }
 
 impl NaiveDateExt for NaiveDate {
-    fn from_str_pt(date: &str, separator: char) -> Result<NaiveDate, Error> {
+    fn from_str_pt(date: &str, separator: char) -> Result<NaiveDate, ParsingError> {
         let segments: Vec<_> = date.split(separator).collect();
 
         if segments.len() != 3 {
-            return Err(anyhow!("Invalid date {}", date));
+            return Err(ParsingError::MissingData);
         }
 
         let date = format!("{}-{}-{}", segments[2], segments[1], segments[0]);
