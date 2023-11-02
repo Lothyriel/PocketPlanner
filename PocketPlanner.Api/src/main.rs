ca157465217374d922f31eaf610f80d375596c84
fastjonh <fastjonh@gmail.com>
2023-11-02 07:50:36 -0300

    fixing for axum crate version

--- PocketPlanner.Api/src/main.rs
+++ PocketPlanner.Api/src/main.rs
@@ -1,21 +1,20 @@
-use axum::{routing, Router};
+use std::net::SocketAddr;
+
 use log::error;
+use pocket_planner::api;
 
 #[tokio::main]
 async fn main() {
-    if let Err(err) = start().await {
+    tracing_subscriber::fmt::init();
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    let server = axum::Server::bind(&addr).serve(api::router().into_make_service());
+
+    if let Err(err) = server.await {
         error!("{}", err);
     }
 }
 
-async fn start() -> Result<(), std::io::Error> {
-    tracing_subscriber::fmt::init();
-
-    let app = Router::new().route("/health", routing::get(|| async { "healthy!" }));
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-
-    axum::serve(listener, app).await
-}
-
+#[allow(dead_code)]
 enum ResponseError {}
