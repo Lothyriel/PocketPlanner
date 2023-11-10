22d2db9af65343e11c4dd72993e8d9ad530376c9
fastjonh <fastjonh@gmail.com>
2023-11-09 23:53:48 -0300

    adding summary route

--- PocketPlanner.Api/src/api/user/mod.rs
+++ PocketPlanner.Api/src/api/user/mod.rs
@@ -1,5 +1,12 @@
-use axum::Router;
+use axum::{routing, Extension, Json, Router};
+
+use super::auth::UserClaims;
 
 pub fn router() -> Router {
-    Router::new()
+    Router::new().route("/summary", routing::get(handler))
+}
+
+#[axum_macros::debug_handler]
+pub async fn handler(Extension(user_claims): Extension<UserClaims>) -> Json<String> {
+    Json(user_claims.email)
 }
