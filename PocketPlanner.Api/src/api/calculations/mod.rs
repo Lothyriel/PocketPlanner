a8f26173186d5e3cd8271cc7a08dd186bb42ee64
fastjonh <fastjonh@gmail.com>
2023-11-24 19:31:46 -0300

    adding endpoint for installments saving

--- PocketPlanner.Api/src/api/calculations/mod.rs
+++ PocketPlanner.Api/src/api/calculations/mod.rs
@@ -1,7 +1,10 @@
+mod installments;
 mod wage_deduction;
 
 use axum::{routing, Router};
 
 pub fn router() -> Router {
-    Router::new().route("/wage-deduction", routing::get(wage_deduction::handler))
+    Router::new()
+        .route("/wage-deduction", routing::get(wage_deduction::handler))
+        .route("/installments", routing::get(installments::handler))
 }
