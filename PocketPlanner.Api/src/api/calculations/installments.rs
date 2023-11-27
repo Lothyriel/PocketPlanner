f6d49acbf39c2da4d407ba94fba9631d5128186e
fastjonh <fastjonh@gmail.com>
2023-11-27 20:22:26 -0300

    adding savings handler

--- PocketPlanner.Api/src/api/calculations/installments.rs
+++ PocketPlanner.Api/src/api/calculations/installments.rs
@@ -1,6 +1,5 @@
 use axum::{extract::Query, Json};
-use rust_decimal::{prelude::FromPrimitive, Decimal};
-use rust_decimal_macros::dec;
+use rust_decimal::Decimal;
 
 #[derive(serde::Deserialize)]
 pub struct Params {
@@ -9,79 +8,9 @@ pub struct Params {
 }
 
 #[derive(serde::Serialize)]
-pub struct InstallmentModel {
-    savings: Decimal,
-    nubank: Decimal,
-    picpay: Decimal,
-}
+pub struct InstallmentsModel {}
 
 #[axum_macros::debug_handler]
-pub async fn handler(params: Query<Params>) -> Json<InstallmentModel> {
+pub async fn handler(params: Query<Params>) -> Json<InstallmentsModel> {
     todo!()
-}
-
-fn calculo(
-    capital_inicial: Decimal,
-    meses: u16,
-    valor_mensal: Decimal,
-    produto: String,
-) -> Decimal {
-    const valorSelic: f32 = 12.25;
-    const ValorTesouro2027: f32 = valorSelic + 0.1736;
-    const valorCDI: f32 = valorSelic - 0.1;
-    let mut porcentagemCDIProduto = 100.;
-    let mut taxaMensal = 0.0;
-    let mut tempoFinal = 1;
-    let mut montante = Decimal::ZERO;
-    let mut valorMensalAcumulado = valor_mensal * Decimal::from_u16(meses).unwrap();
-    let mut valorTotalInvestido = capital_inicial + valorMensalAcumulado;
-
-    if (produto == "poupanca") {
-        if (valorSelic > 8.5) {
-            taxaMensal = 0.5;
-        } else {
-            taxaMensal = valorSelic * 70. / 100. / 12.;
-        }
-    } else if (produto == "nubank") {
-        porcentagemCDIProduto = 100.;
-        taxaMensal = (valorCDI * porcentagemCDIProduto) / 100. / 12.;
-    } else if (produto == "picpay") {
-        porcentagemCDIProduto = 102.0;
-        taxaMensal = (valorCDI * porcentagemCDIProduto) / 100. / 12.;
-    } else if (produto == "tesouro") {
-        taxaMensal = ValorTesouro2027 / 12.;
-    } else if (produto == "selic") {
-        taxaMensal = ValorTesouro2027 / 12.;
-    } else if (produto == "fgts") {
-        taxaMensal = 3. / 12.;
-    } else {
-        taxaMensal = (valorSelic * 70.) / 100. / 12.;
-    }
-    taxaMensal /= 100.;
-
-    while tempoFinal <= meses {
-        montante = capital_inicial * (dec!(1) + Decimal::from_f32(taxaMensal).unwrap());
-        capital_inicial = montante + valor_mensal;
-        tempoFinal += 1;
-    }
-    montante += valor_mensal;
-
-    let produto_bruto = montante;
-    let produto_ir = get_ir(valorTotalInvestido, produto_bruto, meses);
-    let produto_liquido = produto_bruto - produto_ir;
-
-    todo!()
-}
-
-fn get_ir(invested: Decimal, gross: Decimal, months: u16) -> Decimal {
-    let ir_fee = match months {
-        0..=6 => dec!(22.5),
-        7..=12 => dec!(20.0),
-        13..=23 => dec!(17.5),
-        24.. => dec!(15.0),
-    };
-
-    let revenue = gross - invested;
-
-    ir_fee * revenue / dec!(100)
 }
---
+++ PocketPlanner.Api/src/api/calculations/savings.rs
@@ -0,0 +1,101 @@
+use axum::{extract::Query, Json};
+use rust_decimal::{prelude::FromPrimitive, Decimal};
+use rust_decimal_macros::dec;
+
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct Params {
+    pub initial: Decimal,
+    pub contribution: Decimal,
+    pub months: u16,
+}
+
+#[derive(serde::Serialize)]
+pub struct SavingsModel {
+    revenue: Revenue,
+    params: Params,
+}
+
+#[derive(serde::Serialize, Debug)]
+pub struct Revenue {
+    pub savings: Decimal,
+    pub nubank: Decimal,
+    pub picpay: Decimal,
+    pub treasure: Decimal,
+    pub selic: Decimal,
+    pub fgts: Decimal,
+}
+
+struct SavingSimulation {
+    total_invested: Decimal,
+    savings: Decimal,
+}
+
+const SELIC: f64 = 12.25;
+const CDI: f64 = SELIC - 0.1;
+const TREASURE_2027: f64 = SELIC + 0.1736;
+const SAVINGS_RATE: f64 = if SELIC > 8.5 {
+    0.5
+} else {
+    SELIC * 70. / 100. / 12.
+};
+
+#[axum_macros::debug_handler]
+pub async fn handler(params: Query<Params>) -> Json<SavingsModel> {
+    let revenue = get_revenue(&params.0);
+
+    Json(SavingsModel {
+        params: params.0,
+        revenue,
+    })
+}
+
+pub fn get_revenue(params: &Params) -> Revenue {
+    let c = |monthly_fee| {
+        let s = get_gross(params, monthly_fee);
+        s.savings - get_ir(s.total_invested, s.savings, params.months)
+    };
+
+    Revenue {
+        savings: c(SAVINGS_RATE),
+        nubank: c((CDI * 100.) / 100. / 12.),
+        picpay: c((CDI * 102.) / 100. / 12.),
+        treasure: c((TREASURE_2027) / 12.),
+        selic: c((TREASURE_2027) / 12.),
+        fgts: get_gross(params, 3. / 12.).savings,
+    }
+}
+
+fn get_gross(params: &Params, monthly_rate: f64) -> SavingSimulation {
+    let monthly_rate = Decimal::from_f64(monthly_rate).unwrap();
+
+    let mut savings = params.initial;
+
+    for _ in 0..params.months {
+        savings *= dec!(1) + monthly_rate / dec!(100);
+        savings += params.contribution;
+    }
+
+    savings += params.contribution;
+
+    SavingSimulation {
+        savings,
+        total_invested: {
+            let initial = params.initial;
+            let contribution = params.contribution;
+            initial + contribution * Decimal::from_u16(params.months).unwrap()
+        },
+    }
+}
+
+fn get_ir(invested: Decimal, gross: Decimal, months: u16) -> Decimal {
+    let ir_fee = match months {
+        0..=6 => dec!(22.5),
+        7..=12 => dec!(20.0),
+        13..=23 => dec!(17.5),
+        24.. => dec!(15.0),
+    };
+
+    let revenue = gross - invested;
+
+    ir_fee * revenue / dec!(100)
+}
