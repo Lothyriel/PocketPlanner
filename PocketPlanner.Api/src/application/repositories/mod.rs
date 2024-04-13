d15b768f784f0fe9c6121ef6994ee22c1ffdc7a5
João Xavier <fastjonh@gmail.com>
2024-04-13 01:22:44 -0300

    adding func to get client

--- PocketPlanner.Api/src/application/repositories/mod.rs
+++ PocketPlanner.Api/src/application/repositories/mod.rs
@@ -1,1 +1,15 @@
+use mongodb::{error::Result, options::ClientOptions, Client};
+
 mod transaction;
+
+pub async fn get_mongo_client() -> Result<Client> {
+    dotenv::dotenv().ok();
+
+    let connection_string = std::env::var("MONGO_CONNECTION_STRING").unwrap_or_else(|_| {
+        "mongodb://pocket-planner:P@SSW0RD@localhost/?retryWrites=true".to_string()
+    });
+
+    let options = ClientOptions::parse(connection_string).await?;
+
+    Client::with_options(options)
+}
