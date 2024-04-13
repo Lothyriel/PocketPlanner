82bf27cded7aaf153cbf08532727b3631c7d72ec
João Xavier <fastjonh@gmail.com>
2024-04-13 05:04:08 -0300

    shorter names

--- PocketPlanner.Api/src/api/calculations/installments.rs
+++ PocketPlanner.Api/src/api/calculations/installments.rs
@@ -4,9 +4,9 @@ use rust_decimal_macros::dec;
 
 #[derive(serde::Deserialize, serde::Serialize)]
 pub struct Params {
-    financed_value: Decimal,
+    financed: Decimal,
     installments: u16,
-    upfront_value: Decimal,
+    upfront: Decimal,
 }
 
 const CDI: Decimal = dec!(12.35);
@@ -19,10 +19,10 @@ pub struct InstallmentsModel {
 
 #[axum_macros::debug_handler]
 pub async fn handler(Query(params): Query<Params>) -> Json<InstallmentsModel> {
-    let discount = params.financed_value - params.upfront_value;
-    let installment_value = params.financed_value / Decimal::from(params.installments);
+    let discount = params.financed - params.upfront;
+    let installment_value = params.financed / Decimal::from(params.installments);
 
-    let mut remaining = params.financed_value;
+    let mut remaining = params.financed;
     let mut earnings = Decimal::ZERO;
 
     for _ in 0..params.installments {
