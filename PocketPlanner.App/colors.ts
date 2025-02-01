fb18a833b9053e60e3c3b5a1119029efa7bced94
João Xavier <fastjonh@gmail.com>
2025-02-01 20:38:06 -0300

    removing app
    I want to avoid js if possible...
    and also wanna try something new with "server" rendering HTML

--- PocketPlanner.App/colors.ts
+++
@@ -1,3 +0,0 @@
-export const colors = {
-  main: 'rgb(5, 158, 94)'
-}
--- PocketPlanner.App/polyfills.ts
+++
@@ -1,6 +0,0 @@
-import { decode, encode } from "base-64"
-
-(function base64() {
-  global.btoa = encode
-  global.atob = decode
-})()
