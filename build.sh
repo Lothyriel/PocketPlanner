489f5dabde918681fd260be5fe380145b9d864cf
João Xavier <fastjonh@gmail.com>
2025-03-02 05:16:01 -0300

    updating build script
    we only need to build the api to x64
    
    and we dont need ts in wasm output

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-cargo build --release
+cargo build --release -p api
 
-wasm-pack build ./src/app --target web --release
+wasm-pack build ./src/app --target web --no-typescript --release
 
 cp ./src/app/pkg/app_bg.wasm ./public
 cp ./src/app/pkg/app.js ./public
