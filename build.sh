8acd7e808dd16e9f58a134f5687519ec59bd0cc9
João Xavier <fastjonh@gmail.com>
2025-03-03 01:15:34 -0300

    this should do it...
    I was never actually copying the right generated wasm files, I was
    copying the ones that were on my local /public folder...

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-cargo build --release -p api
-
 wasm-pack build ./src/app --target web --no-typescript --release
 
 cp ./src/app/pkg/app_bg.wasm ./public
 cp ./src/app/pkg/app.js ./public
 
-echo "Published .wasm"
+echo "Published .wasm artifacts"
