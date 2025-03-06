7ac8b0046fdb99de3638d21dcadd6709c1d8eb80
João Xavier <fastjonh@gmail.com>
2025-03-06 18:34:52 -0300

    build wasm pack in debug mode

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-wasm-pack build ./src/app --target web --no-typescript --release
+wasm-pack build ./src/app --target web --no-typescript
 
 cp ./src/app/pkg/app_bg.wasm ./public
 cp ./src/app/pkg/app.js ./public
