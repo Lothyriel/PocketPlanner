5a314960828463ef78df299b2215219740440499
João Xavier <fastjonh@gmail.com>
2025-03-08 00:37:55 -0300

    returning the actual js response instead of a string

--- src/app/src/lib.rs
+++ src/app/src/lib.rs
@@ -1,24 +1,50 @@
-use anyhow::{anyhow, Result};
+use std::collections::HashMap;
+
 use axum::{
     body::Body,
-    http::{Method, Request},
+    http::{response::Parts, HeaderMap, HeaderValue, Method, Request},
 };
 use http_body_util::BodyExt;
 use tower::ServiceExt;
-use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+use wasm_bindgen::{prelude::wasm_bindgen, JsError, JsValue};
+use web_sys::{Response, ResponseInit};
 
 #[wasm_bindgen]
-pub async fn render(req: JsValue) -> String {
-    let req: ReqParts = deser(req).expect("Valid request object");
+pub async fn render(method: String, uri: String, form: Option<String>) -> JsResult<Response> {
+    let req = build_request(&method, &uri, form)?;
 
-    let method = Method::from_bytes(req.method.as_bytes()).expect("Valid HTTP method");
+    let res = lib::router().oneshot(req).await?;
 
-    let route = req
-        .route
-        .strip_prefix("/fragments")
-        .expect("strip /fragments prefix");
+    let (parts, body) = res.into_parts();
 
-    let body = if let Some(form) = req.form {
+    let mut body = body.collect().await?.to_bytes().to_vec();
+
+    let options = build_options(parts);
+
+    Response::new_with_opt_u8_array_and_init(Some(body.as_mut()), &options?)
+        .map_err(|_| JsError::new(&format!("Error creating response {method} - {uri}")))
+}
+
+fn build_options(parts: Parts) -> JsResult<ResponseInit> {
+    let options = ResponseInit::new();
+
+    options.set_status(parts.status.as_u16());
+    options.set_headers(&serde_wasm_bindgen::to_value(&get_headers(&parts.headers))?);
+
+    Ok(options)
+}
+
+fn get_headers(header_map: &HeaderMap<HeaderValue>) -> HashMap<&str, Option<&str>> {
+    header_map
+        .iter()
+        .map(|(name, value)| (name.as_str(), value.to_str().ok()))
+        .collect()
+}
+
+fn build_request(method: &str, uri: &str, form: Option<String>) -> JsResult<Request<Body>> {
+    let method = Method::from_bytes(method.as_bytes())?;
+
+    let body = if let Some(form) = form {
         Body::from(form)
     } else {
         Body::empty()
@@ -26,37 +52,14 @@ pub async fn render(req: JsValue) -> String {
 
     let req = Request::builder()
         .header("Content-Type", "application/x-www-form-urlencoded")
-        .uri(route)
+        .uri(uri)
         .method(method)
-        .body(body)
-        .expect("Valid request");
+        .body(body)?;
 
-    let response = lib::fragments::router()
-        .oneshot(req)
-        .await
-        .expect("Valid response");
-
-    let response = response
-        .into_body()
-        .collect()
-        .await
-        .expect("Get response body as bytes")
-        .to_bytes()
-        .to_vec();
-
-    String::from_utf8(response).expect("Valid UTF-8 String")
+    Ok(req)
 }
 
-fn deser<T: serde::de::DeserializeOwned>(val: JsValue) -> Result<T> {
-    serde_wasm_bindgen::from_value(val).map_err(|e| anyhow!("{e}"))
-}
-
-#[derive(serde::Deserialize)]
-struct ReqParts {
-    method: String,
-    route: String,
-    form: Option<String>,
-}
+type JsResult<T> = Result<T, JsError>;
 
 #[wasm_bindgen(start)]
 pub async fn start() {
