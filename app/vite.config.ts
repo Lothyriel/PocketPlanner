8347aca82ed748f81ea27d3ae563294285796db5
João Xavier <fastjonh@gmail.com>
2025-08-09 22:39:30 -0300

    add tailwind

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-})
+export default defineConfig({ plugins: [tailwindcss(), svelte()] });
