11b17db085f883ce635da4bedc6de2109a7d80db
João Xavier <fastjonh@gmail.com>
2025-08-09 23:05:11 -0300

    shadcn-svelte init && add login component

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [tailwindcss(), svelte()] });
+export default defineConfig({
+  plugins: [tailwindcss(), svelte()], resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
+});
